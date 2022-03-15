dce1cd26f170190e581d15893ca21b63585bce74
Sviatoslav27 <sviatoslav.radysh@gmail.com>
2022-03-15 17:53:50 +0200

    Adding Spring config.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -14,9 +14,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
 
@@ -52,3 +52,5 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Vets....");
     }
 }
+
+
